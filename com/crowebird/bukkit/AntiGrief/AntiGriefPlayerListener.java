dc83a0fdfe0fb5fda8f3aae3298a0bda071692ff
Michael Crowe <mecrowe@umich.edu>
2011-02-15 14:03:06 -0500

    Adding files to git

---
+++ com/crowebird/bukkit/AntiGrief/AntiGriefPlayerListener.java
@@ -0,0 +1,18 @@
+package com.crowebird.bukkit.AntiGrief;
+
+import org.bukkit.event.player.PlayerItemEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class AntiGriefPlayerListener extends PlayerListener {
+
+	private AntiGrief plugin;
+	
+	public AntiGriefPlayerListener(AntiGrief plugin_) {
+		this.plugin = plugin_;
+	}
+	
+	public void onPlayerItem(PlayerItemEvent event_) {
+		if (!this.plugin.canBuild(event_.getPlayer(), "antigrief.player.item"))
+			event_.setCancelled(true);
+	}
+}
