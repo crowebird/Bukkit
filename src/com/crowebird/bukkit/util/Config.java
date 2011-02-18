61ef00e743b12ded70b310cbb4740fc60d45b856
Michael Crowe <mecrowe@umich.edu>
2011-02-18 17:20:21 -0500

    Modified configuration reading

--- src/com/crowebird/bukkit/util/Config.java
+++ src/com/crowebird/bukkit/util/Config.java
@@ -13,18 +13,45 @@ import java.util.logging.Logger;
 
 import org.bukkit.util.config.Configuration;
 
+import com.crowebird.bukkit.AntiGrief.AntiGrief;
+
 public class Config {
 
 	protected static final Logger log = Logger.getLogger("Minecraft");
 	
-	public static Configuration read(String path_, String file_) throws Exception {
+	public static class type extends HashMap<String, ArrayList<String>> {
+		private static final long serialVersionUID = 1L;
+	};
+	
+	public static Config.type read(String path_, String file_, Config.type default_) throws Exception {
 		File yml = new File(path_ + File.separator + file_);
 		if (!yml.exists()) throw new IOException("File does not exist!");
 
-		return new Configuration(yml);
+		Config.type hm = new Config.type();
+		String[] keys = default_.keySet().toArray(new String[]{});
+		
+		Configuration config = new Configuration(yml);
+		config.load();
+		for(String key : keys) {
+			ArrayList<String> values = (ArrayList<String>)config.getStringList(key, default_.get(key));
+			hm.put(key, values);
+		}
+
+		return hm;
 	}
 	
-	public static void create(String path_, String file_, HashMap<String, ArrayList<String>> contents_) {
+	public static Config.type getConfig(String path_, String file_, Config.type default_) {
+		try {
+			return Config.read(path_, file_, default_);
+		} catch (Exception ex) {
+			if (ex instanceof IOException)
+				Config.create(path_, file_, default_);
+			log.info(AntiGrief.pdf.getName() + " - using default values!");
+			return default_;
+		}
+	}
+	
+	public static void create(String path_, String file_, Config.type contents_) {
 		try {
 			File f = new File(path_ + File.separator + file_);
 			if (!f.exists()) {
@@ -36,11 +63,12 @@ public class Config {
 			Iterator<Entry<String, ArrayList<String>>> i = contents_.entrySet().iterator();
 			while(i.hasNext()) {
 				Entry<String, ArrayList<String>> e = i.next();
-				out.write(e.getKey() + ":");
-				for (String line : e.getValue()) {
-					out.newLine();
-					out.write("- '" + line + "'");
-				}
+				out.write(e.getKey() + ": ");
+				String line_out = "";
+				ArrayList<String> values = e.getValue();
+				for (String line : values)
+					line_out += (line_out.equals("") ? "" : ",\n") + "'" + line + "'";
+				out.write("[" + line_out + "]");
 				out.newLine();
 			}
 			
