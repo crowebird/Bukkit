9bff8e6292ef18d5152eec2f236786d40f87083d
Michael Crowe <mecrowe@umich.edu>
2011-02-27 20:52:25 -0500

    Updated configuration with abstract types to support configuration files with different types.

--- src/com/crowebird/bukkit/util/Config.java
+++ src/com/crowebird/bukkit/util/Config.java
@@ -1,3 +1,10 @@
+/**
+ * Configuration reader for yml using Bukkit Configuration class
+ * Created by Michael Crowe (crowebird)
+ * 
+ * Feel free to learn from but give credit! :D
+ */
+
 package com.crowebird.bukkit.util;
 
 import java.io.BufferedWriter;
@@ -6,9 +13,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.logging.Level;
+import java.util.Stack;
+import java.util.TreeSet;
 import java.util.logging.Logger;
 
 import org.bukkit.util.config.Configuration;
@@ -17,38 +23,87 @@ public class Config {
 
 	protected static final Logger log = Logger.getLogger("Minecraft");
 	
-	public static class type extends HashMap<String, ArrayList<String>> {
+	private static class AL<T> extends ArrayList<T> {
+		private static final long serialVersionUID = 2L;
+		
+		public Class<?> getParameterized() {
+			return this.getClass();
+		}
+	}
+	
+	public static class ALInteger extends Config.AL<Integer> {
+		private static final long serialVersionUID = 3L;
+		
+		public Class<?> getParameterized() {
+			return Integer.class;
+		}
+	}
+	
+	public static class ALString extends Config.AL<String> {
+		private static final long serialVersionUID = 4L;
+		
+		public Class<?> getParameterized() {
+			return String.class;
+		}
+	}
+	
+	public static class Type extends HashMap<String, Object> {
 		private static final long serialVersionUID = 1L;
+		
+		public boolean has(String key_, Object value_) {
+			if (!this.containsKey(key_)) return false;
+			Object value = this.get(key_);
+
+			if (value instanceof ArrayList<?>) return ((ArrayList<?>) value).contains(value_);
+			else return value.equals(value_);
+		}
 	};
 	
-	public static Config.type read(String path_, String file_, Config.type default_) throws Exception {
+	public static Config.Type read(String plugin_, String path_, String file_, Config.Type default_) throws IOException {
 		File yml = new File(path_ + File.separator + file_);
-		if (!yml.exists()) throw new IOException("File does not exist!");
+		if (!yml.exists()) throw new IOException();
 
-		Config.type hm = new Config.type();
-		String[] keys = default_.keySet().toArray(new String[]{});
+		Config.Type hm = new Config.Type();
+		TreeSet<String> keys = new TreeSet<String>(default_.keySet());
 		
 		Configuration config = new Configuration(yml);
 		config.load();
+		
 		for(String key : keys) {
-			ArrayList<String> values = (ArrayList<String>)config.getStringList(key, default_.get(key));
-			hm.put(key, values);
+			Object value = config.getProperty(key);
+			Object expected = default_.get(key);
+			
+			boolean use_default = false;
+			if (value == null) use_default = true;
+			else {
+				if (value instanceof ArrayList<?> && expected instanceof Config.AL<?>) {
+					ArrayList<?> provided_value = (ArrayList<?>)value;
+					if (provided_value.size() != 0) {
+						if (!provided_value.get(0).getClass().equals(((Config.AL<?>)expected).getParameterized())) use_default = true;
+					}
+				}
+			}
+			if (use_default) {
+				Config.log.warning(plugin_ + " - " + (value == null ? "Missing value" : "Unexpected value for") + " " + key + " in [" + path_ + File.separator + file_ + "], using default.");
+				value = expected;
+			}
+			hm.put(key, value);
 		}
 
 		return hm;
 	}
 	
-	public static Config.type getConfig(String path_, String file_, Config.type default_) {
+	public static Config.Type getConfig(String plugin_, String path_, String file_, Config.Type default_) {
 		try {
-			return Config.read(path_, file_, default_);
+			return Config.read(plugin_, path_, file_, default_);
 		} catch (Exception ex) {
 			if (ex instanceof IOException)
-				Config.create(path_, file_, default_);
+				Config.create(plugin_, path_, file_, default_);
 			return default_;
 		}
 	}
 	
-	public static void create(String path_, String file_, Config.type contents_) {
+	public static void create(String plugin_, String path_, String file_, Config.Type contents_) {
 		try {
 			File f = new File(path_ + File.separator + file_);
 			if (!f.exists()) {
@@ -57,24 +112,46 @@ public class Config {
 			}
 			
 			BufferedWriter out = new BufferedWriter(new FileWriter(path_ + File.separator + file_));
-			Iterator<Entry<String, ArrayList<String>>> i = contents_.entrySet().iterator();
-			while(i.hasNext()) {
-				Entry<String, ArrayList<String>> e = i.next();
-				out.write(e.getKey() + ": ");
+			TreeSet<String> keys = new TreeSet<String>(contents_.keySet());		
+			
+			Stack<String> s = new Stack<String>();
+			for(String key : keys) {
+				String[] levels = key.split("\\.");
+				for(int x = 0; x < levels.length - 1; ++x) {
+					try {
+						if (!s.get(x).equals(levels[x])) for (int y = x; y < s.size(); ++y) s.removeElementAt(y);
+						else continue;
+					} catch (IndexOutOfBoundsException ex) { /**/ }
+					out.write(Config.getTab(x) + levels[x] + ": ");
+					out.newLine();
+					s.push(levels[x]);
+				}
+				if (levels.length == 1) s.clear();
+
+				Object value = contents_.get(key);
+				out.write(Config.getTab(s.size()) + levels[levels.length - 1] + ": ");
 				String line_out = "";
-				ArrayList<String> values = e.getValue();
-				for (String line : values)
-					line_out += (line_out.equals("") ? "" : ",\n") + "'" + line + "'";
-				out.write("[" + line_out + "]");
+				if (value instanceof ArrayList<?>) {
+					ArrayList<?> list = (ArrayList<?>)value;
+					for (Object line : list)
+						line_out += (line_out.equals("") ? "" : ",\n" + Config.getTab(s.size() + 1)) + (line instanceof String ? "'" : "") + line + (line instanceof String ? "'" : "");
+					line_out = "[" + line_out + "]";
+				} else line_out = (value instanceof String ? "'" : "") + value + (value instanceof String ? "'" : "");
+				out.write(line_out);
 				out.newLine();
 			}
 			
 			out.flush();
 			out.close();
-			log.log(Level.INFO, "File created: " + path_ + File.separator + file_ + "!");
+			Config.log.info(plugin_ + " - Configuration file created [" + path_ + File.separator + file_ + "]!");
 		} catch (Exception ex) {
-			log.log(Level.WARNING, "Unable to create " + path_ + File.separator + file_ + "!");
+			Config.log.warning(plugin_ + " - Unable to properly create config [" + path_ + File.separator + file_ + "]!");
 		}
 	}
 	
+	private static String getTab(int depth_) {
+		String output = "";
+		for (int x = 0; x < depth_; ++x) output += "  ";
+		return output;
+	}
 }
