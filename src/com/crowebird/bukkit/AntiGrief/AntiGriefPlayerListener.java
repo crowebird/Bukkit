382a44913dad6e8954768f4e8c04e44b76c21898
Michael Crowe <mecrowe@umich.edu>
2011-03-06 19:23:45 -0500

    Added missing item use and pickup commands back

--- src/com/crowebird/bukkit/AntiGrief/AntiGriefPlayerListener.java
+++ src/com/crowebird/bukkit/AntiGrief/AntiGriefPlayerListener.java
@@ -1,13 +1,14 @@
 package com.crowebird.bukkit.AntiGrief;
 
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerItemEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class AntiGriefPlayerListener extends PlayerListener {
 
-	@SuppressWarnings("unused")
 	private final AntiGrief plugin;
 	
 	public AntiGriefPlayerListener(AntiGrief plugin_) {
@@ -15,16 +16,16 @@ public class AntiGriefPlayerListener extends PlayerListener {
 	}
 	
 	public void onPlayerItem(PlayerItemEvent event_) {
-		@SuppressWarnings("unused")
+		ItemStack item = event_.getItem();
 		Player player = event_.getPlayer();
-		//if (!this.plugin.access(player, "player.item.use", event_.getItem().getType().getId()))
-		//	event_.setCancelled(true);
+		if (!this.plugin.access(player, "player.item.use", player.getLocation(), item.getTypeId()))
+			event_.setCancelled(true);
 	}
 	
 	public void onPlayerPickupItem(PlayerPickupItemEvent event_ ) {
-		@SuppressWarnings("unused")
+		Item item = event_.getItem();
 		Player player = event_.getPlayer();
-		//if (!this.plugin.access(player, "player.item.pickup", event_.getItem().getItemStack().getTypeId(), true)) {
-		//	event_.setCancelled(true);
+		if (!this.plugin.access(player, "player.item.pickup", event_.getItem().getLocation(), item.getItemStack().getTypeId(), true))
+			event_.setCancelled(true);
 	}
 }
